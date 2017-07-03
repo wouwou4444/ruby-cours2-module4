f53aa2fee78284cc905fd46dac550a6663d82210
wouwou4444 <wouwou4444@outlook.com>
2017-07-03 12:10:17 +0200

    init 3

--- todolists/app/controllers/todo_items_controller.rb
+++ todolists/app/controllers/todo_items_controller.rb
@@ -1,4 +1,5 @@
 class TodoItemsController < ApplicationController
+  before_action :set_todo_list
   before_action :set_todo_item, only: [:show, :edit, :update, :destroy]
 
   helper_method :set_todo_list
@@ -15,7 +16,7 @@ class TodoItemsController < ApplicationController
 
   # GET /todo_items/new
   def new
-    @todo_item = TodoItem.new
+    @todo_item = @todo_list.todo_items.new
   end
 
   # GET /todo_items/1/edit
@@ -25,11 +26,11 @@ class TodoItemsController < ApplicationController
   # POST /todo_items
   # POST /todo_items.json
   def create
-    @todo_item = TodoItem.new(todo_item_params)
+    @todo_item = @todo_list.todo_items.new(todo_item_params)
 
     respond_to do |format|
       if @todo_item.save
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully created.' }
+        format.html { redirect_to @todo_list , notice: 'Todo item was successfully created.' }
         format.json { render :show, status: :created, location: @todo_item }
       else
         format.html { render :new }
@@ -43,7 +44,7 @@ class TodoItemsController < ApplicationController
   def update
     respond_to do |format|
       if @todo_item.update(todo_item_params)
-        format.html { redirect_to @todo_item, notice: 'Todo item was successfully updated.' }
+        format.html { redirect_to @todo_list, notice: 'Todo item was successfully updated.' }
         format.json { render :show, status: :ok, location: @todo_item }
       else
         format.html { render :edit }
@@ -57,7 +58,7 @@ class TodoItemsController < ApplicationController
   def destroy
     @todo_item.destroy
     respond_to do |format|
-      format.html { redirect_to todo_items_url, notice: 'Todo item was successfully destroyed.' }
+      format.html { redirect_to @todo_list, notice: 'Todo item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -67,13 +68,13 @@ class TodoItemsController < ApplicationController
   private
 
     def set_todo_list
-      @todo_list = TodoList.find(:todo_list_id)
+      @todo_list = TodoList.find(params[:todo_list_id])
     end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_todo_item
-      @todo_item = TodoItem.find(params[:id])
+      @todo_item = @todo_list.todo_items.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
